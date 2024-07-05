66ca72d5c80a05e632a454f1cd3d1ba67f20c02c
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-06 07:39:14 +0800

    menambahkan route untuk controller post

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route resource
+Route::resource('/post', \App\Http\Controllers\PostController::class);
