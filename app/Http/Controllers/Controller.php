d9e2528731b9650bd2bffb99b23f866e13c08900
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-05 17:10:11 +0800

    create controller post

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    /**
+     * index
+     * @return void
+     */
+
+    public function index()
+    {
+        //get post
+        $post = Post::lates()->pagination(5);
+
+        //render view with post
+        return view('post.index',compact('post'));
+    }
 }
