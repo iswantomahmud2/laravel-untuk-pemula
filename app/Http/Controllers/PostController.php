6a8c8a7ff6a24394bb500dd0d8b5a4b769d7ed6b
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-06 07:35:42 +0800

    perbaiki kesalahan penulisan method di controller lain

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * index
+     * @return void
+     */
+
+    public function index()
+    {
+        //get post
+        $post = Post::lates()->pagination(5);
+
+        //render view with post
+        return view('post.index', compact('post'));
+    }
 }
