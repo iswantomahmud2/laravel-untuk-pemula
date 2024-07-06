afed65258d12dbb0c9bf9d5cb7a9b22f43d890d4
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-06 16:18:46 +0800

    menambahkan method edit dan update pada postcontoller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -57,5 +58,46 @@ class PostController extends Controller
 
         //return view
         return view('posts.show', compact('post'));
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //validate form update
+        $request->validate([
+            'image' => 'image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts' . $post->image);
+
+            //update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        } else {
+            //update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil diubah!']);
     }
 }
