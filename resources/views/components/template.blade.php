6fefa07c166ff35496e05083c70c9df03912c278
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-12 17:16:51 +0800

    revisi dan tidak jadi menggunakan google sheet app google

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -36,8 +36,7 @@
         <!-- End Sidebar -->
 
         <!-- Top Navbar -->
-        {{-- @include('components.navbar_top') --}}
-        
+        @include('components.navbar_top')
         <!-- End Top Navbar -->
         <div class="content-page">
             <div class="container-fluid">
